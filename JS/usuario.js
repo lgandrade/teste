3bf746e161d5f67c27ab9bda26b443207fb3666c
lgandrade <113061621+lgandrade@users.noreply.github.com>
2023-08-06 18:36:58 -0300

    Add files via upload

---
+++ JS/usuario.js
@@ -0,0 +1,10 @@
+document.documentElement.style.cursor = 'none';
+
+setTimeout(function() {
+	// Mostra o cursor novamente
+	document.documentElement.style.cursor = 'auto';
+  }, 10000);
+
+setTimeout(function() {
+	window.location.href ='inicio.html';
+  }, 8200);
